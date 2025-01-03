ddf79e09dc11cc483571e205dc0bd98bd9d81289
Your Name <alexandermorgan364@gmail.com>
2025-01-03 18:59:12 -0500

    Header Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import React from "react";
-import Message from "./components/Message";
-import Favorite from "./components/Favorite";
-import ListGroup from "./components/ListGroup";
 import "./App.css";
-
+import Navbar from "./components/Navbar/Navbar";
+import Header from "./containers/Header/Header";
 const App: React.FC = () => {
   const items = ["New York", "San Francisco", "Tokyo"];
   return (
-    <div>
-      <Message who="Alex" />
-      <Favorite what="Chilling" />
-      <ListGroup items={items} />
+    <div className="App">
+      <Navbar />
+      <Header />
     </div>
   );
 };
