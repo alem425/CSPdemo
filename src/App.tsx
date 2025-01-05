99e3eb148077a56b141022a2196a2d81e6051960
Your Name <alexandermorgan364@gmail.com>
2025-01-05 04:30:43 -0500

    Pre-Cards Commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./containers/Header/Header";
+import Orgs from "./components/TrustedOrgs/Orgs";
+import CSPmodel from "./containers/CPSmodel/CSPmodel";
 const App: React.FC = () => {
   const items = ["New York", "San Francisco", "Tokyo"];
   return (
     <div className="App">
       <Navbar />
       <Header />
+      <Orgs />
+      <CSPmodel />
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 // Create root element
 const root = ReactDOM.createRoot(document.getElementById("app") as HTMLElement);
