644ecd32d28c964e1283487edaae04d46cdfad0c
Your Name <alexandermorgan364@gmail.com>
2025-01-07 02:28:46 -0500

    First Page Completed

---
+++ src/Pages/Home.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Home.css";
+import Navbar from "../components/Navbar/Navbar";
+import Header from "../containers/Header/Header";
+import Orgs from "../components/TrustedOrgs/Orgs";
+import CSPmodel from "../containers/CPSmodel/CSPmodel";
+const App: React.FC = () => {
+  const items = ["New York", "San Francisco", "Tokyo"];
+  return (
+    <div className="App">
+      <Navbar />
+      <Header />
+      <Orgs />
+      <CSPmodel />
+    </div>
+  );
+};
+
+export default App;
